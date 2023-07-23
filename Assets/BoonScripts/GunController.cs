4e7fd2ecda43a1876ba90b353e3479b6bcbca229
BoonYiLua <121838754+BoonYiLua@users.noreply.github.com>
2023-07-23 22:19:34 +0800

    Worked on bullet fx and magazine for gun works

--- Assets/BoonScripts/GunController.cs
+++ Assets/BoonScripts/GunController.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 public class GunController : MonoBehaviour {
     public GameObject bulletPrefab;
     public Transform firePoint;
-    public int maxAmmo = 30;
+    public int magazineSize = 10; // The maximum ammo capacity in the magazine.
+    public int maxAmmo = 100; // The maximum total ammo.
     public float fireRate = 0.2f;
     public float reloadTime = 1.5f;
     public float bulletSpeed = 20f;
 
-    private int currentAmmo;
+    private int currentAmmo; // The current total ammo.
+    private int currentMagazineAmmo; // The current ammo in the magazine.
     private bool isReloading = false;
     private float nextFireTime = 0f;
 
-
     private void Start() {
         currentAmmo = maxAmmo;
-
-     
+        currentMagazineAmmo = magazineSize;
     }
 
     private void Update() {
-        if (isReloading)
-            return;
-
-        if (currentAmmo <= 0) {
+        if (currentMagazineAmmo <= 0 && !isReloading && Input.GetKeyDown(KeyCode.R)) {
             StartCoroutine(Reload());
             return;
         }
@@ -34,43 +31,45 @@ public class GunController : MonoBehaviour {
             nextFireTime = Time.time + .1f / fireRate;
             Shoot();
         }
-
-        if (Input.GetKeyDown(KeyCode.R)) {
-            StartCoroutine(Reload());
-        }
     }
 
     private void Shoot() {
-        // Instantiate the bullet prefab at the FirePoint position and rotation
-        GameObject newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        if (currentMagazineAmmo > 0) {
+            // Instantiate the bullet prefab at the FirePoint position and rotation
+            GameObject newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        // Get the Rigidbody component of the bullet.
-        Rigidbody bulletRigidbody = newBullet.GetComponent<Rigidbody>();
+            // Get the Rigidbody component of the bullet.
+            Rigidbody bulletRigidbody = newBullet.GetComponent<Rigidbody>();
 
-        // Calculate the direction to shoot (straight, in front)
-        Vector3 shootDirection = firePoint.forward;
+            // Calculate the direction to shoot (straight, in front)
+            Vector3 shootDirection = firePoint.forward;
 
-        // Add a force to the bullet to move it in the calculated direction with a certain speed.
-        bulletRigidbody.AddForce(shootDirection * bulletSpeed, ForceMode.VelocityChange);
+            // Add a force to the bullet to move it in the calculated direction with a certain speed.
+            bulletRigidbody.AddForce(shootDirection * bulletSpeed, ForceMode.VelocityChange);
 
-        // Reduce ammo
-        currentAmmo--;
+            // Reduce ammo from the magazine
+            currentMagazineAmmo--;
+        }
     }
 
     private IEnumerator Reload() {
-        isReloading = true;
+        if (currentAmmo > 0 && currentMagazineAmmo < magazineSize) {
+            isReloading = true;
 
-        // Perform reload animation or logic here
-        Debug.Log("Reloading");
+            int ammoToReload = Mathf.Min(magazineSize - currentMagazineAmmo, currentAmmo);
+            currentMagazineAmmo += ammoToReload;
+            currentAmmo -= ammoToReload;
 
-        yield return new WaitForSeconds(reloadTime);
+            // Perform reload animation or logic here
+            Debug.Log("Reloading...");
 
-        currentAmmo = maxAmmo;
-        isReloading = false;
+            yield return new WaitForSeconds(reloadTime);
+
+            isReloading = false;
+        }
     }
 
     public void AddAmmo(int amount) {
         currentAmmo = Mathf.Clamp(currentAmmo + amount, 0, maxAmmo);
     }
 }
-
