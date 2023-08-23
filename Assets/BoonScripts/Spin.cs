3b6852c92642b3d13dcc29912db89e2a9b1f86ef
BoonYiLua <121838754+BoonYiLua@users.noreply.github.com>
2023-08-23 16:40:11 +0800

    Baking class

--- Assets/BoonScripts/Spin.cs
+++ Assets/BoonScripts/Spin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    float rotationSpeed =270f;
+    float rotationSpeed =180f;
     // Start is called before the first frame update
     void Start()
     {
