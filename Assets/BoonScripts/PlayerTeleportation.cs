3fc44ca370ed095506327010bdd50b725c3ec814
BoonYiLua <121838754+BoonYiLua@users.noreply.github.com>
2023-08-23 14:56:59 +0800

    IT WORKS

--- Assets/BoonScripts/PlayerTeleportation.cs
+++ Assets/BoonScripts/PlayerTeleportation.cs
@@ -3,37 +3,49 @@ using UnityEngine;
 public class PlayerTeleportation : MonoBehaviour {
     public Transform targetTeleporter; // The other teleporter to teleport to.
     public float detectionRadius = 2.0f; // Radius within which the player can be detected.
+    public bool onTeleporter = false;
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
-            // Check if the player is within the detection radius of the teleporter.
-            if (IsPlayerNearby()) {
-                TeleportPlayer();
-            }
-        }
+    [SerializeField] CharacterController player;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject);
+        if (other.CompareTag("Player")) 
+        {
+            player = other.GetComponent<CharacterController>();
+
+            onTeleporter = true;
+        }                    
+
     }
 
-    private bool IsPlayerNearby() {
-        // Find all colliders within the detection radius of the teleporter.
-        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
-
-        // Check if any of the colliders belong to an object with the "Player" tag.
-        foreach (Collider collider in colliders) {
-            if (collider.CompareTag("Player")) {
-                return true; // Player is nearby.
-            }
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            onTeleporter = false;
         }
-
-        return false; // Player is not nearby.
     }
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.E) && onTeleporter) {
+
+            Debug.Log("Wdwdw");
+            TeleportPlayer();
+        } 
+         
+                   
+    }
+
+    
 
     private void TeleportPlayer() {
         // Find the player using the "Player" tag.
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
+            
+    
         if (player != null) {
+            Debug.Log("wdwdwDD");
             // Teleport the player to the target teleporter's position.
-            player.transform.position = targetTeleporter.position;
+            player.GetComponent<CharacterController>().enabled = false;
+            player.gameObject.transform.position = new Vector3(targetTeleporter.position.x, player.transform.position.y,targetTeleporter.position.z);
+            player.GetComponent<CharacterController>().enabled = true;
         }
     }
 }
